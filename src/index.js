a815614e713b86948cd85a026af4bfba6975d572
Seungmin Lee <reemrev21@gmail.com>
2021-06-19 16:44:47 +0900

    Create store

--- src/index.js
+++ src/index.js
@@ -1,27 +1,33 @@
+import { createStore } from "redux";
+// 1. data를 넣을 수 있는 장소를 생성한다. 
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-// 딱 한번만 작동한다. 
-number.innerText = count;
+// createStore 사용하려면 필요하다. 
+// data를 바꾸고 수정할 수 있다. 
+// Reducer 함수만 데이터를 수정하고 바꿀 수 있다.
+// action은 함수를 부를 때 쓰는 두번째 인자이자 인수이다. 
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "Minus") {
+    return count - 1;
+  } else {
+    return 0;
+  }
+};
+// 스토어 생성 -> 데이터 저장
+const countStore = createStore(countModifier);
 
-// handleAdd, handleMinus는 값을 업데이트 하는 작업은 하지 않기 때문에
-// 값을 업데이트 할 수 있는 함수를 만들어준다. 
-const updateText = () => {
-  number.innerText = count;
- }
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "Minus" });
 
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
- };
-
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
- };
-
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+console.log(countStore.getState());
