d12a2050a735e027d1cf075db003c7d033a65a94
Seungmin Lee <reemrev21@gmail.com>
2021-06-19 16:57:16 +0900

    counter done

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+// 0으로 초기화
+number.innerHTML = 0;
+
 // createStore 사용하려면 필요하다. 
 // data를 바꾸고 수정할 수 있다. 
 // Reducer 함수만 데이터를 수정하고 바꿀 수 있다.
@@ -18,16 +21,24 @@ const countModifier = (count = 0, action) => {
     return 0;
   }
 };
+
 // 스토어 생성 -> 데이터 저장
 const countStore = createStore(countModifier);
 
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "Minus" });
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
 
-console.log(countStore.getState());
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+}
+
+const handleMinus = () => {
+  countStore.dispatch({ type: "Minus" });
+}
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
+
