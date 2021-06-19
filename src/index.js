eac01642ffd76ba11bd49d4a8d87b37830b7750d
Seungmin Lee <reemrev21@gmail.com>
2021-06-19 17:59:32 +0900

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
 import { createStore } from "redux";
-// 1. data를 넣을 수 있는 장소를 생성한다. 
 
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-// 0으로 초기화
 number.innerHTML = 0;
 
-// createStore 사용하려면 필요하다. 
-// data를 바꾸고 수정할 수 있다. 
-// Reducer 함수만 데이터를 수정하고 바꿀 수 있다.
-// action은 함수를 부를 때 쓰는 두번째 인자이자 인수이다. 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "Minus") {
-    return count - 1;
-  } else {
-    return 0;
+  // if 보다 switch를 사용하는것이 훨씬 간단하다!
+  switch (action.type) {
+    case ADD:
+      return count + 1
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
-// 스토어 생성 -> 데이터 저장
 const countStore = createStore(countModifier);
 
 const onChange = () => {
@@ -32,11 +30,11 @@ const onChange = () => {
 countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD });
 }
 
 const handleMinus = () => {
-  countStore.dispatch({ type: "Minus" });
+  countStore.dispatch({ type: MINUS });
 }
 
 add.addEventListener("click", handleAdd);
