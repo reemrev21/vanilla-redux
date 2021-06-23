06f79351054802d63b9eaced67f2ee258dc69981
Seungmin Lee <reemrev21@gmail.com>
2021-06-24 00:11:24 +0900

    react-redux todo

---
+++ src/components/ToDo.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { connect } from "react-redux";
+import { actionCreators } from "../store";
+import { Link } from "react-router-dom";
+
+function ToDo({ text, onBtnClick, id }) {
+  return (
+    <li>
+      <Link to={`/${id}`}>
+        {text}
+      </Link>
+      <button onClick={onBtnClick}>DEL</button>
+    </li>
+  );
+};
+
+function mapStateToProps(dispatch, ownProps) {
+  return {
+    onBtnClick: () => dispatch(actionCreators.deleteToDo(ownProps.id))
+  }
+}
+export default connect(null, mapStateToProps)(ToDo);
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
+import { actionCreators } from "../store";
+import ToDo from "../components/ToDo";
 
-function Home() {
+function Home({toDos, addToDo}) {
   const [text, setText] = useState("");
 
   function onChange(e) {
@@ -9,20 +12,46 @@ function Home() {
 
   function onSubmit(e) {
     e.preventDefault();
+    addToDo(text);
     setText("");
   }
    
   return (
     <>
-      <h1> To do</h1>
+      <h1> To do!! </h1>
       <form onSubmit={onSubmit}>
         <input type="text" value={text} onChange={onChange} />
         <button>Add</button>
       </form>
-      <ul></ul>
+      <ul>
+        {toDos.map(
+          toDo =>
+            <ToDo {...toDo} key={toDo.id} />
+        )}
+      </ul>
     </>
   );
 }
 
-export default Home;
+// store와 connect
+// getCurrentState
+// redux state로 부터 component에 prop으로 전달
+// getStore랑 동일한 역할?!
+function mapStateToProp(state) {
+  return { toDos: state };
+}
+
+// props로 보낼 수 있다.
+function mapDispatchToProps(dispatch) {
+  return {
+    addToDo: text => dispatch(actionCreators.addToDo(text))
+  };
+}
+
+// 이 순서 안지켜주면 에러남 ㅋㅋ
+export default connect(mapStateToProp, mapDispatchToProps)(Home);
+
+// 둘 중하나만 사용할 경우 이렇게 표현
+// export default connect(null, mapDispatchToProps)(Home);
+
  
