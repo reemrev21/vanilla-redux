9bca324fb6c68491891ffd698effa7a63ba87e8e
Seungmin Lee <reemrev21@gmail.com>
2021-06-24 12:07:18 +0900

    redux-toolkit

--- src/store.js
+++ src/store.js
@@ -1,39 +1,31 @@
-import { createStore } from "redux";
+import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
 
-const addToDo = text => {
-  return {
-    type: ADD,
-    text
-  }
-}
+// mutate 하기 훨씬 좋음
+// 1. 새로운 것을 return 할 수 있다.
+// redux toolkit은 immer 기반? 라서 mutate 가능.. 
+// 2. *** state를 mutate 할 수 있다. ***
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    // return 할 때는 새로운 state 여야함
+    // 이 경우에 push()는 아무것도 리턴하지 않음 state mutate만함
+    // 그래서 이렇게 작성
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteToDo]: (state, action) =>
+    // filter는 리턴한다. 
+    state.filter(toDo => toDo.id !== action.payload)
+})
 
-const deleteToDo = id => {
-  return {
-    type: DELETE,
-    id: parseInt(id)
-  }
-}
-  
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter(toDo => toDo.id !== action.id);
-    default:
-      return state;
-  }
-}
-
-const store = createStore(reducer);
+// redux develop tool 이용 가능
+// state에 패스워드에 저장하면 안됨ㅋㅋ 다 보인다!
+const store = configureStore(reducer);
 
 export const actionCreators = {
   addToDo,
   deleteToDo
 };
-export default store;
 
-// store.getState() 는 현재의 state를 전달해준다. 
+export default store;
