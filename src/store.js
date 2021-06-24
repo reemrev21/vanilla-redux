8a45a34c44b8469e8bcbc6010ed224b8b7c2d733
Seungmin Lee <reemrev21@gmail.com>
2021-06-24 12:21:37 +0900

    use createslice

--- src/store.js
+++ src/store.js
@@ -1,31 +1,21 @@
-import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const addToDo = createAction("ADD");
-const deleteToDo = createAction("DELETE");
-
-// mutate 하기 훨씬 좋음
-// 1. 새로운 것을 return 할 수 있다.
-// redux toolkit은 immer 기반? 라서 mutate 가능.. 
-// 2. *** state를 mutate 할 수 있다. ***
-const reducer = createReducer([], {
-  [addToDo]: (state, action) => {
-    // return 할 때는 새로운 state 여야함
-    // 이 경우에 push()는 아무것도 리턴하지 않음 state mutate만함
-    // 그래서 이렇게 작성
-    state.push({ text: action.payload, id: Date.now() });
-  },
-  [deleteToDo]: (state, action) =>
-    // filter는 리턴한다. 
-    state.filter(toDo => toDo.id !== action.payload)
+// reducer와 action 모두 제공
+const toDos = createSlice({
+  name: "toDoReducer",
+  initialState: [],
+  reducers: {
+    add: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() })
+    },
+    remove: (state, action) => 
+      state.filter(toDo => toDo.id !== action.payload)
+  }
 })
+
+export const { add, remove } = toDos.actions;
 
 // redux develop tool 이용 가능
 // state에 패스워드에 저장하면 안됨ㅋㅋ 다 보인다!
-const store = configureStore(reducer);
+export default configureStore({reducer : toDos.reducer});
 
-export const actionCreators = {
-  addToDo,
-  deleteToDo
-};
-
-export default store;
